a8af1aa77488c4cf5e41b8b4a41f6c8240a9a594
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-05-24 11:53:18 +0300

    j

--- src/test/java/ru/mail/go/api/GoMailTest.java
+++ src/test/java/ru/mail/go/api/GoMailTest.java
@@ -1,37 +1,39 @@
 package ru.mail.go.api;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.jsoup.Connection;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import ru.mail.go.api.domain.SearchResultCard;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+
 public class GoMailTest {
 @Test
     public void testGetSearch() throws IOException {
 
-  Connection.Response  response = Jsoup.connect("https://go.mail.ru/search?q=Минск").execute();
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options=new ChromeOptions();
+    options.addArguments("headless");
+    options.addArguments("disabled-gpu");
+    WebDriver driver=new ChromeDriver(options);
+    driver.get("https://go.mail.ru/search?q=Минск");
+    String pageSource = driver.getPageSource();
+    Document doc =Jsoup.parse(pageSource);
+   // System.out.println(doc.toString());
+    Elements elements = doc.selectXpath("//*[contains(@class,'Link-root SnippetResultTitle')]/ancestor::li");
 
-    int i = response.statusCode();
-   String s = response.statusMessage();
-    Assert.assertEquals(200,i);
-    Document doc = response.parse();
-    System.out.println(doc.toString());
-   // String body = Jsoup.connect("https://go.mail.ru/search?q=Минск").execute().body();
-   // JSONArray objects = new JSONArray(body);
-
-    Elements elementById = doc.selectXpath("//*[@id=\"reactHeaderRoot\"]/div/div[1]/div[1]/a[1]/svg");
     List<SearchResultCard> resultCards = new ArrayList<>();
-    /*for (Element byClass : elementsByClass) {
+    for (Element byClass : elements) {
         SearchResultCard searchResultCard=new SearchResultCard(
            byClass.getElementsByClass("SnippetResultTitle-title result__title").text(),
                 byClass.getElementsByClass("Link-root SnippetResultInfo-url Link-orange Link-hoverable").text()
@@ -39,8 +41,9 @@ public class GoMailTest {
         resultCards.add(searchResultCard);
 
     }
-    System.out.println(resultCards);*/
-
+    System.out.println(resultCards);
+    assertTrue(resultCards.stream().anyMatch(s->s.getTitle().contains("Минск — Википедия")));
+    assertTrue(resultCards.stream().anyMatch(s->s.getUrl().contains("ru.wikipedia.org›wiki/Минск")));
 
 }
 }
